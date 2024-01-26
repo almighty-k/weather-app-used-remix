fc265a7e6354ee493e1dc67b752d0795cd4a7d1e
almighty-k <takada.chopin.work@gmail.com>
2024-01-26 15:36:32 +0900

    feat: current weather取得用のloaderを実装

--- app/routes/_main.weather_forecasts.tsx
+++ app/routes/_main.weather_forecasts.tsx
@@ -1,3 +1,28 @@
+import { defer } from "@vercel/remix";
+
+import type { CurrentResponse } from "~/api.types";
+import { API_ENDPOINT } from "~/config.server";
+
 export default function WeatherForecasts() {
   return <p>週間天気予報ページ</p>;
+}
+
+export async function loader() {
+  // TODO: 検索クエリから都市名（または緯度、軽度）を取得するよう実装。一旦、固定値で東京を指定
+  const location = "Tokyo";
+  const currentWeatherPromise = fetchCurrentWeather({ location });
+  return defer({
+    currentWeatherPromise
+  });
+}
+
+async function fetchCurrentWeather({
+  location
+}: {
+  location: string;
+}): Promise<CurrentResponse> {
+  const res = await fetch(
+    `${API_ENDPOINT}/current.json?key=${process.env.WHETHER_API_KEY}&q=${location}`
+  );
+  return await res.json();
 }
