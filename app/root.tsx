92800187953d1e6db2af66071487ef4c3fc60b5d
almighty-k <takada.chopin.work@gmail.com>
2024-01-26 11:40:15 +0900

    feat: vercelデプロイに適用するよう設定実装

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+
 import {
   Links,
   LiveReload,
@@ -8,6 +8,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@vercel/remix";
+
+export const config = { runtime: "edge" };
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
