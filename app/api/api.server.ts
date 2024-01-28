bbe1c49358344c7941f90e94f4ce1fa6300dd85e
almighty-k <takada.chopin.work@gmail.com>
2024-01-28 09:25:14 +0900

    feat: 予報天気取得関数を実装

--- app/api/api.server.ts
+++ app/api/api.server.ts
@@ -1,5 +1,5 @@
 import { API_ENDPOINT } from "../config.server";
-import type { CurrentResponse } from "./api.types";
+import type { CurrentResponse, ForecastResponse } from "./api.types";
 
 export async function fetchCurrentWeather({
   location
@@ -8,6 +8,19 @@ export async function fetchCurrentWeather({
 }): Promise<CurrentResponse> {
   const res = await fetch(
     `${API_ENDPOINT}/current.json?key=${process.env.WHETHER_API_KEY}&q=${location}`
+  );
+  return await res.json();
+}
+
+export async function fetchForecastWeather({
+  location,
+  days = 9 // 1週間分という仕様だが、9日分あっても困らないのと、3日ずつ表示する際にUI崩れをふせぐために9日分取得する
+}: {
+  location: string;
+  days?: number;
+}): Promise<ForecastResponse> {
+  const res = await fetch(
+    `${API_ENDPOINT}/forecast.json?key=${process.env.WHETHER_API_KEY}&q=${location}&days=${days}`
   );
   return await res.json();
 }
