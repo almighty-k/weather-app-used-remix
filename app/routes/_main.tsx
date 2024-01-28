5dd04dcf8eb76b54ea217e2944e7e92ddd77ff93
almighty-k <takada.chopin.work@gmail.com>
2024-01-28 17:15:43 +0900

    feat: WeatherAPIのクレジット表記を実装

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -4,8 +4,17 @@ import { Outlet } from "@remix-run/react";
 
 export default function Main() {
   return (
-    <div className={classes.wrapper}>
-      <Outlet />
-    </div>
+    <>
+      <div className={classes.wrapper}>
+        <Outlet />
+      </div>
+
+      <p className={classes.credit}>
+        Powered by{" "}
+        <a href="https://www.weatherapi.com/" title="Free Weather API">
+          WeatherAPI.com
+        </a>
+      </p>
+    </>
   );
 }
