0c09eb4f2af67f744fff347e60cb8dbb4e4d1848
almighty-k <takada.chopin.work@gmail.com>
2024-01-28 13:17:39 +0900

    feat: getMonthAndDate関数を日時入力にも対応させるよう実装

--- app/utils.ts
+++ app/utils.ts
@@ -1,9 +1,10 @@
 /**
  * 日付から年を取り除き, 日月のみの形式で返す
- * @param date 例:"2024-01-27"
+ * @param dateTime 例:"2024-01-27 or 2024-01-27 13:08"
  * @returns 例:"27/01"
  */
-export function getMonthAndDate(date: string) {
+export function getMonthAndDate(dateTime: string) {
+  const [date] = dateTime.split(" ");
   // 配列の第一引数は使用しないため、_で受け取り、lintエラーを無視する
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, month, day] = date.split("-");
