bd92966791dfbbbf91baa91a02edbdfc1e43a050
almighty-k <takada.chopin.work@gmail.com>
2024-01-28 10:06:35 +0900

    feat: SearchInputに検索機能およびデフォルト値をもたせるよう実装

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,16 +1,31 @@
 import classes from "./input.module.css";
 
 import type { InputHTMLAttributes } from "react";
+import { Form, useSearchParams, useSubmit } from "@remix-run/react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
 export function SearchInput({ label, ...props }: InputProps) {
+  const submit = useSubmit();
+  const [searchParams] = useSearchParams();
+
   return (
-    <div className={classes.wrapper}>
+    <Form
+      className={classes.wrapper}
+      onChange={(e) => {
+        submit(e.currentTarget, { replace: true });
+      }}
+    >
       <label htmlFor={props.id}>{label}</label>
-      <input {...props} className={classes.input} />
-    </div>
+      <input
+        name={props.name}
+        placeholder="Input Example: tokyo or 35.6894,139.6917"
+        defaultValue={searchParams.get("location") || ""}
+        {...props}
+        className={classes.input}
+      />
+    </Form>
   );
 }
